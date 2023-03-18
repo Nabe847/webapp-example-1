dae7cbc08880ccdad9edc283cb9d883801a9759c
manabe takumi <manabetakumi02@gmail.com>
2023-03-18 18:46:46 +0900

    add: express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello world');
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(3000, () => {
+  console.log("Server listen on port 3000");
+});
